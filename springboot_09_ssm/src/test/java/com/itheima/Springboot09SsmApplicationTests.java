c4f319c710d4c2d86b2ca58b23f10f2416ae49e4
gsnpoor <2366106257@qq.com>
2023-04-19 20:53:47 +0800

    2023.4.19

---
+++ springboot_09_ssm/src/test/java/com/itheima/Springboot09SsmApplicationTests.java
@@ -0,0 +1,27 @@
+package com.itheima;
+
+import com.itheima.domain.Book;
+import com.itheima.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class Springboot09SsmApplicationTests {
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void testGetById(){
+        Book book = bookService.getById(2);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testGetAll() {
+        List<Book> all = bookService.getAll();
+        System.out.println(all);
+    }
+}
